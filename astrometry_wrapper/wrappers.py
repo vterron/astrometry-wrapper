748c818d0384fb525eb2a0178ee557e3da9efc3d
Víctor Terrón <vterron@iaa.es>
2015-04-20 20:21:42 +0200

    Fix import

--- astrometry_wrapper/wrappers.py
+++ astrometry_wrapper/wrappers.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import .commands
+from . import commands
 
 def find_sources(path, type = 'fits'):
     """ Detect astronomical objects in a FITS image. """
